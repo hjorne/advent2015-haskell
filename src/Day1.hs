5892a2878b1d7f05511826c4152ee2486058d0ec
Joe Horne <joeh19@gmail.com>
2019-07-28 13:05:04 -0400

    Completed day 1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,10 +1,35 @@
-module Day1 (day1) where
+module Day1 where
+
+import System.IO
+import Debug.Trace
 
 day1 :: IO () 
 day1 = print "Executing day 1" >> part1 >> part2
 
 part1 :: IO ()
-part1 = print "Executing part 1"
+part1 = readFile "input/day1.txt" >>= print . solution1
 
 part2 :: IO ()
-part2 = print "Executing part 2"
+part2 = readFile "input/day1.txt" >>= print . solution2
+        
+solution1 :: String -> Int
+solution1 = finalFloor . stringToInts
+
+parenToInt :: Char -> Int
+parenToInt '(' = 1
+parenToInt ')' = -1
+
+stringToInts :: String -> [Int]
+stringToInts = fmap parenToInt
+
+finalFloor :: [Int] -> Int
+finalFloor = sum
+
+countToBasement :: [Int] -> Int
+countToBasement = snd . foldl go (0, 0)
+    where go (floor, index) dfloor
+            | floor == -1 = (floor, index)
+            | otherwise   = (floor + dfloor, index + 1)
+
+solution2 :: String -> Int
+solution2 = countToBasement . stringToInts
